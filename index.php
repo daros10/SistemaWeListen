b760158f6f2d77da5276ca29ad86db1550dc77c1
Unknown <daro.wla@hotmail.es>
2018-02-08 00:37:56 -0500

    change index

--- index.php
+++ index.php
@@ -16,51 +16,150 @@
 <!DOCTYPE html>
 <html>
   <head>
+
+
     <meta charset="utf-8">
-    <title>WeListen</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
+    <!-- Bootstrap Core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
+  <!-- MetisMenu CSS -->
+  <link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
 
+  <!-- Custom CSS -->
+  <link href="dist/css/sb-admin-2.css" rel="stylesheet">
 
+  <!-- Morris Charts CSS -->
+  <link href="vendor/morrisjs/morris.css" rel="stylesheet">
+
+  <!-- Custom Fonts -->
+  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
   </head>
   <body>
-    <?php require 'partials/header.php' ?>
 
-    <?php if(!empty($user)): ?>
-      <br> Bienvenido <?= $user['email']; ?>
-      <br>Has iniciado sesión correctamente
-      <a href="logout.php">
-        Logout
-      </a>
+    <!--CODE PHP-->
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand">WeListen</a>
+            </div>
+            <!-- /.navbar-header -->
+            <ul class="nav navbar-top-links navbar-right">
+                <!-- /.dropdown PERFIL DE USUARIO -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                        <?php if(!empty($user)): ?>
+                        <?= $user['email']; ?>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+            <!-- /.navbar-top-links -->
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            <div class="input-group custom-search-form">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                            </div>
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+                            <a href="index.php"><i class="fa fa-music"></i> Inicio</a>
+                        </li>
+                        <li>
+                            <a href="musicaDisponible.html"><i class="fa fa-play-circle"></i> Tu música</a>
+                        </li>
+
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Música</h1>
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-sm-4" >
+                          <img id="img-album" src="img/music.png"  width="300" alt="">
+                          <audio id="player" controls="" ></audio>
+                        </div>
+                        <div class="col-sm-5">
+                          <button class="btn btn-succes" id="shuffle"><i class="fa fa-random"></i></button>
+                          <ul class="list-group" id="playlist"></ul>
+                        </div>
+
+                      </div>
+
+                    </div>
 
 
 
-      <!--navbar-->
 
-      <nav class="navbar navbar-light" style="background-color: #F8F8FF;" >
-        <!-- Navbar content -->
-        <a class="navbar-brand light" href="components/home/home.php">Home</a>
-        <a class="navbar-brand light" href="components/about/about.php">About</a>
-        <a class="navbar-brand light" href="#">Tag2</a>
-        <a class="navbar-brand light" href="#">Tag3</a>
-      </nav>
-      <!---->
 
-    <?php else: ?>
-      <h1> Inicia Sesión o Registrate</h1>
 
-      <a href="login.php">Inicia Sesión</a> o
-      <a href="signup.php">Registrate</a>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+      </div>
+
+
+
+
+      <!--renderiza directamente la pagina de login-->
+    <?php else:
+        header('Location: login.php');
+      ?>
     <?php endif; ?>
 
-    <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="vendor/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="dist/js/sb-admin-2.js"></script>
+    <script src="js/app.js"></script>
 
 
 
