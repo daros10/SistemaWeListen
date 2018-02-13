545c3679e0ad2db7fa65e02d5854b424f2908f62
Unknown <daro.wla@hotmail.es>
2018-02-13 11:33:43 -0500

    modify aplication.js

--- js/aplication.js
+++ js/aplication.js
@@ -6,7 +6,7 @@ var audio;
 var music;
 
 function getSongs(){
-  $.getJSON("js/app.json",function(mjson){
+  $.getJSON("js/appSongs.json",function(mjson){
     music = mjson;
     genList(music);
 
@@ -20,7 +20,8 @@ function playSong(id){
 
 function genList(music){
   $.each(music.songs,function(i,song){
-    $('#playlist').append('<li class="list-group-item" id="'+i+'">'+song.name+'</li>')
+    $('#playlist').append('<li class="list-group-item" id="'+i+'">'+song.name+' - '+song.artista+'<button type="button" class="btn btn-primary pull-right btn-sm">Seleccionar</button>'+'</li>');
+
   });
   $('#playlist li').click(function(){
     var selectedsong = $(this).attr('id');
