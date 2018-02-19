7077984ff68e132c52fdabb59a220b59f9da3bfb
daros10 <daro.wla@hotmail.es>
2018-02-19 17:18:40 -0500

    added email

--- signup.php
+++ signup.php
@@ -18,18 +18,25 @@
 <link rel="shortcut icon" type="image/x-icon" href="pantallaprueba/img/iconlogoWeListen.ico">
 <!DOCTYPE html>
 <html>
-<header class="masthead"> 
+<<<<<<< Updated upstream
+<header class="masthead">
   <head> <meta charset="utf-8">
     <title>Regístrate</title>
-	
+
    <link rel="shortcut icon" type="image/x-icon" href="pantallaprueba/img/iconlogoWeListen.ico">
-   
+
+=======
+<header class="masthead">
+  <head>
+    <meta charset="utf-8">
+    <title>Registrate</title>
+>>>>>>> Stashed changes
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
- 
+
     <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
- 
-    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet"> 
-    <link rel="stylesheet" href="assets/css/style.css"> 
+
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
 
@@ -43,12 +50,12 @@
     <span>o <a href="login.php">Inicia Sesión</a></span>
 
     <form action="signup.php" method="POST">
-      <input name="email" type="text" placeholder="Ingresa tu email" required>
+      <input name="email" type="email" placeholder="Ingresa tu email" required>
       <input name="password" type="password" placeholder="Ingresa tu contraseña" required>
       <input name="confirm_password" type="password" placeholder="Confirma tu contraseña" required>
       <input type="submit" value="Registrarse">
     </form>
 
   </body>
-   </header> 
+   </header>
 </html>
